132af341288d3869c1aa4bb7debe6f65c25ea688
Valli250397 <valli@srinsofttech.com>
2025-12-27 00:21:35 +0530

    added ratehomepage

--- frontend/src/redux/slices/rate.jsx
+++ frontend/src/redux/slices/rate.jsx
@@ -8,40 +8,59 @@ import { dispatch } from '../store';
 // ----------------------------------------------------------------------
 
 const initialState = {
-  isLoading: false,
-  error : null,
-  rateSuccess: false,
-  rateTableData: [],
-  rateSearchObj: {},
+    isLoading: false,
+    error: null,
+    rateSuccess: false,
+    rateTableData: [],
+    rateSearchObj: {},
 };
 
 const slice = createSlice({
-  name: 'rate',
-  initialState,
-  reducers: {
-    hasError(state, action) {
-      state.isLoading = false;
-      state.error = 'error';
+    name: 'rate',
+    initialState,
+    reducers: {
+        hasError(state, action) {
+            state.isLoading = false;
+            state.error = 'error';
+        },
+        // START LOADING
+        startLoading(state) {
+            state.isLoading = true;
+            state.rateSuccess = false;
+            state.error = null;
+        },
+        getRateDashboarddataSuccess(state, action) {
+            state.isLoading = false;
+            state.rateTableData = [
+                {
+                    id: 1,
+                    rateID: "RID10001",
+                    origin: "ORD",
+                    originZipCode: "10001, 10002",
+                    destination: "Los Angeles",
+                    destinationZipCode: "90001, 90002",
+                    status: "Active",
+                    min: '140.00',
+                    '100': '.29 per lb',
+                    '1000': '.24 per lb',
+                    '3000': '.21 per lb',
+                    '5000': '.19 per lb',
+                    '10000': '.17 per lb',
+                    max: '1755.00',
+                    effectiveDate: "01-30-2024",
+                    expiryDate: "01-30-2026"
+                }
+            ];
+        },
+        setRateSearchObj(state, action) {
+            state.rateSearchObj = action.payload;
+        },
+
     },
-    // START LOADING
-    startLoading(state) {
-      state.isLoading = true;
-      state.rateSuccess = false;
-      state.error = null;
-    },
-    getRateDashboarddataSuccess(state, action) {
-        state.isLoading = false;
-        state.rateTableData = [];
-    },
-    setRateSearchObj(state, action) {
-      state.rateSearchObj = action.payload;
-    },
-     
-  },
 });
 
 export const {
-  setRateSearchObj,
+    setRateSearchObj,
 } = slice.actions;
 export default slice.reducer;
 
@@ -50,13 +69,13 @@ export default slice.reducer;
 
 // ----------------------------------------------------------------------
 export function getRateDashboardData() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-    //   const response = await axios.get('/ratedashboarddata');
-      dispatch(slice.actions.getRateDashboarddataSuccess([]));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
+    return async () => {
+        dispatch(slice.actions.startLoading());
+        try {
+            //   const response = await axios.get('/ratedashboarddata');
+            dispatch(slice.actions.getRateDashboarddataSuccess([]));
+        } catch (error) {
+            dispatch(slice.actions.hasError(error));
+        }
+    };
 }
--- frontend/src/sections/rate/RatehomePage.jsx
+++ frontend/src/sections/rate/RatehomePage.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
+import {
+  Box, Stack, Typography, Button, Divider, Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
+import { ErrorBoundary } from 'react-error-boundary';
 
-// ----------------------------------------------------------------------
+// shared components
+import ErrorFallback from '../shared/ErrorBoundary';
+// ----------------------------------------------------------------
 
 export default function RatehomePage() {
+  const logError = (error, info) => {
+    // Use an error reporting service here
+    console.error("Error caught:", info);
+    console.log(error);
+  };
   return (
     <>
-      Rate Maintenance home page from section
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onError={logError}
+        onReset={() => {
+          // Optional: reset app state here if necessary before retry
+          console.log("Error boundary reset triggered");
+        }}
+      >
+        <Box>
+         <Typography sx={{ fontSize: '18px', fontWeight: 600, mt:2 }}>Rate Maintenance</Typography>
+        </Box>
+      </ErrorBoundary>
     </>
   );
 }
